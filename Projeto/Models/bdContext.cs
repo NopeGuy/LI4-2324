45e318afceb9673575c6fcaa66950b546f3adcd3
Bernazad <62028005+HBernaH@users.noreply.github.com>
2024-01-20 01:25:45 +0000

    Bue :honey_pot:
    Falta Histórico, Vendas, Minhas Salas e Criar Sala

--- Projeto/Models/bdContext.cs
+++ Projeto/Models/bdContext.cs
@@ -35,10 +35,6 @@ public partial class bdContext : DbContext
 
     public virtual DbSet<vendedor_has_sala> vendedor_has_sala { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=3L1T3B00K;Initial Catalog=li4;Integrated Security=True;Encrypt=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<admin>(entity =>
